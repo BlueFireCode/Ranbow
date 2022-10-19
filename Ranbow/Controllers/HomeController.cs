2ac619a6c1cfecd6ae7f5f5543f08f9ed0678d2a
JulianusIV <ivjulianus@gmail.com>
2022-10-19 20:11:48 +0200

    Add icon to discord embed

--- Ranbow/Controllers/HomeController.cs
+++ Ranbow/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace Ranbow.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Icon()
+        {
+            return File("~/images/Logos/bluefire-01.png", "image/png");
+        }
     }
 }
