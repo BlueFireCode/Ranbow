f87af59b077e6340595300d2e092819d35ae70cb
JulianusIV <ivjulianus@gmail.com>
2024-03-13 01:05:34 +0100

    update scope options helper function

--- web-app/src/logic/helpers.rs
+++ web-app/src/logic/helpers.rs
@@ -2,10 +2,8 @@ pub fn u8_to_sight_name(sight: u8) -> String {
     match sight {
         0 => String::from("Iron Sights"),
         1 => String::from("Any 1.0x Sight variant"),
-        2 => String::from("Scope 1.5x"),
-        3 => String::from("Scope 2.0x"),
-        4 => String::from("Scope 2.5x"),
-        5 => String::from("Scope 3.0x"),
+        2 => String::from("Scope 2.5x"),
+        3 => String::from("Scope 3.5x"),
         _ => format!("Unkown sight: {}", sight)
     }
 }
